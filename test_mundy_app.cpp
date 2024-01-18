7e0fa9e3d012350079cc6905d6f2724412fc4f9b
Christopher Edelmaier <cedelmaier@flatironinstitute.org>
2024-01-18 14:30:51 -0500

    Moved unit tests into a format that is compatible with ctest, also setup more testing directory hierarchies

---
+++ test_mundy_app.cpp
@@ -0,0 +1,10 @@
+// C++ core libs
+
+#include <memory>     // for std::shared_ptr, std::unique_ptr
+#include <stdexcept>  // for std::logic_error, std::invalid_argument
+#include <string>     // for std::string
+#include <vector>     // for std::vector
+
+int main(int argc, char **argv) {
+  return 0;
+}
