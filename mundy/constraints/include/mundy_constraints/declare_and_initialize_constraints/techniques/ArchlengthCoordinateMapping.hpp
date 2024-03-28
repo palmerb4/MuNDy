b521abda09113778c762f7f38c2c89035ddd382d
Bryce Palmer <palmerb4@wit.edu>
2024-03-28 15:01:52 -0400

    Toward a formal chain-of-springs declaration and initialization routine.

---
+++ mundy/constraints/include/mundy_constraints/declare_and_initialize_constraints/techniques/ArchlengthCoordinateMapping.hpp
@@ -0,0 +1,151 @@
+// @HEADER
+// **********************************************************************************************************************
+//
+//                                          Mundy: Multi-body Nonlocal Dynamics
+//                                           Copyright 2023 Flatiron Institute
+//                                                 Author: Bryce Palmer
+//
+// Mundy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Mundy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Mundy. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// **********************************************************************************************************************
+// @HEADER
+
+#ifndef MUNDY_CONSTRAINTS_DECLARE_AND_INITIALIZE_CONSTRAINTS_TECHNIQUES_ARCHLENGTHCOORDINATEMAPPING_HPP_
+#define MUNDY_CONSTRAINTS_DECLARE_AND_INITIALIZE_CONSTRAINTS_TECHNIQUES_ARCHLENGTHCOORDINATEMAPPING_HPP_
+
+/// \file ArchlengthCoordinateMapping.hpp
+/// \brief An interface and some concrete implementations for mapping archlength to coordinates.
+
+// C++ core libs
+#include <array>  // for std::array
+#include <cmath>  // for std::sin, std::cos, std::sqrt
+
+// Mundy includes
+#include <mundy_math/Matrix3.hpp>     // for mundy::math::Matrix3
+#include <mundy_math/Quaternion.hpp>  // for mundy::math::Quaternion
+#include <mundy_math/Vector3.hpp>     // for mundy::math::Vector3
+
+namespace mundy {
+
+namespace constraints {
+
+namespace declare_and_initialize_constraints {
+
+namespace techniques {
+
+/// @brief An interface for mapping grid indices to coordinates.
+class ArchlengthCoordinateMapping {
+ public:
+  /// \brief Virtual destructor.
+  virtual ~ArchlengthCoordinateMapping() = default;
+
+  /// \brief Get the coordinate corresponding to a given archlength index in [0, num_nodes-1]
+  /// \param archlength_index The archlength index.
+  /// \return The corresponding coordinate.
+  virtual std::array<double, 3> get_grid_coordinate(const size_t &archlength_index) const = 0;
+};  // class ArchlengthCoordinateMapping
+
+/// @brief Straight line with given center, length, and orientation.
+class StraightLine : public ArchlengthCoordinateMapping {
+ public:
+  /// Constructor
+  StraightLine(const size_t num_nodes, const double center_x, const double center_y, const double center_z,
+               const double length, const double orientation_x, const double orientation_y, const double orientation_z)
+      : num_nodes_(num_nodes),
+        center_x_(center_x),
+        center_y_(center_y),
+        center_z_(center_z),
+        length_(length),
+        orientation_x_(orientation_x),
+        orientation_y_(orientation_y),
+        orientation_z_(orientation_z) {
+  }
+
+  /// \brief Get the grid coordinate corresponding to a given grid index.
+  /// \param archlength_index The archlength index.
+  /// \return The corresponding coordinate.
+  std::array<double, 3> get_grid_coordinate(const size_t &archlength_index) const override {
+    const double x = center_x_ + (static_cast<double>(archlength_index) / static_cast<double>(num_nodes_ - 1) - 0.5) *
+                                     length_ * orientation_x_;
+    const double y = center_y_ + (static_cast<double>(archlength_index) / static_cast<double>(num_nodes_ - 1) - 0.5) *
+                                     length_ * orientation_y_;
+    const double z = center_z_ + (static_cast<double>(archlength_index) / static_cast<double>(num_nodes_ - 1) - 0.5) *
+                                     length_ * orientation_z_;
+    return {x, y, z};
+  }
+
+ private:
+  size_t num_nodes_;
+  double center_x_;
+  double center_y_;
+  double center_z_;
+  double length_;
+  double orientation_x_;
+  double orientation_y_;
+  double orientation_z_;
+};  // class StraightLine
+
+/// @brief Helix with given center, radius, length and the axis to the helix's center.
+class Helix : public ArchlengthCoordinateMapping {
+ public:
+  /// Constructor
+  Helix(const size_t &num_nodes, const double &radius, const double &length, const double &center_x,
+        const double &center_y, const double &center_z, const double &axis_x, const double &axis_y,
+        const double &axis_z)
+      : num_nodes_(num_nodes),
+        radius_(radius),
+        length_(length),
+        center_{center_x, center_y, center_z},
+        axis_{axis_x, axis_y, axis_z} {
+    // Not that we don't trust you or anything, but we need to make sure the axis is normalized.
+    axis_ /= mundy::math::norm(axis_);
+
+    // We need to find two orthonormal vectors to the axis of the helix.
+    // We can do this by finding an arbitrary vector that is not parallel to the axis, taking the cross product
+    // with the normal, and normalizing the result. This gives us a vector that is orthogonal to the axis.
+    // By taking the cross product of the axis and this vector, we get a second vector that is orthogonal to both.
+    const mundy::math::Vector3<double> ihat(1.0, 0.0, 0.0);
+    const mundy::math::Vector3<double> jhat(0.0, 1.0, 0.0);
+    basis_vector0_ = mundy::math::norm(mundy::math::cross(axis_, ihat)) > 1.0e-12 ? ihat : jhat;
+    basis_vector0_ /= mundy::math::norm(basis_vector0_);
+    basis_vector1_ = mundy::math::norm(mundy::math::cross(axis_, basis_vector0_));
+    basis_vector1_ /= mundy::math::norm(basis_vector1_);
+  }
+
+  /// \brief Get the grid coordinate corresponding to a given grid index.
+  /// \param archlength_index The archlength index.
+  /// \return The corresponding coordinate.
+  std::array<double, 3> get_grid_coordinate(const size_t &archlength_index) const override {
+    const double angle = 2.0 * M_PI * static_cast<double>(archlength_index) / static_cast<double>(num_nodes_);
+    const auto pos_circle = center_ + radius_ * (basis_vector0_ * std::cos(angle) + basis_vector1_ * std::sin(angle));
+    const double shift = (static_cast<double>(archlength_index) / static_cast<double>(num_nodes_ - 1) - 0.5) * length_;
+    const auto pos = pos_circle + shift * axis_;
+    return {pos[0], pos[1], pos[2]};
+  }
+
+ private:
+  size_t num_nodes_;
+  double radius_;
+  double length_;
+  mundy::math::Vector3<double> center_;
+  mundy::math::Vector3<double> axis_;
+  mundy::math::Vector3<double> basis_vector0_;
+  mundy::math::Vector3<double> basis_vector1_;
+};  // class Helix
+
+}  // namespace techniques
+
+}  // namespace declare_and_initialize_constraints
+
+}  // namespace constraints
+
+}  // namespace mundy
+
+#endif  // MUNDY_CONSTRAINTS_DECLARE_AND_INITIALIZE_CONSTRAINTS_TECHNIQUES_ARCHLENGTHCOORDINATEMAPPING_HPP_
---
+++ mundy/constraints/src/mundy_constraints/declare_and_initialize_constraints/techniques/ChainOfSprings.cpp
@@ -0,0 +1,245 @@
+// @HEADER
+// **********************************************************************************************************************
+//
+//                                          Mundy: Multi-body Nonlocal Dynamics
+//                                           Copyright 2023 Flatiron Institute
+//                                                 Author: Bryce Palmer
+//
+// Mundy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+// Mundy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Mundy. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+// **********************************************************************************************************************
+// @HEADER
+
+/// \file ChainOfSprings.cpp
+/// \brief Definition of DeclareAndInitConstraints' ChainOfSprings technique.
+
+// C++ core libs
+#include <iostream>  // for std::cout, std::endl
+#include <memory>    // for std::shared_ptr, std::unique_ptr
+#include <string>    // for std::string
+#include <vector>    // for std::vector
+
+// External libs
+#include <openrand/philox.h>
+
+// Trilinos libs
+#include <Teuchos_ParameterList.hpp>      // for Teuchos::ParameterList
+#include <stk_mesh/base/Entity.hpp>       // for stk::mesh::Entity
+#include <stk_mesh/base/Field.hpp>        // for stk::mesh::Field, stl::mesh::field_data
+#include <stk_mesh/base/GetEntities.hpp>  // for stk::mesh::count_entities
+#include <stk_search/BoundingBox.hpp>     // for stk::search::Box
+#include <stk_search/CoarseSearch.hpp>    // for stk::search::coarse_search
+#include <stk_search/SearchMethod.hpp>    // for stk::search::KDTREE
+
+// Mundy libs
+#include <mundy_constraints/HookeanSprings.hpp>  // for mundy::constraints::HookeanSprings
+#include <mundy_constraints/declare_and_initialize_constraints/techniques/ArchlengthCoordinateMapping.hpp>  // for mundy::constraints::...::ArchlengthCoordinateMapping
+#include <mundy_constraints/declare_and_initialize_constraints/techniques/ChainOfSprings.hpp>  // for mundy::constraints::...::ChainOfSprings
+#include <mundy_core/throw_assert.hpp>                                                         // for MUNDY_THROW_ASSERT
+#include <mundy_mesh/BulkData.hpp>  // for mundy::mesh::BulkData
+
+namespace mundy {
+
+namespace constraints {
+
+namespace declare_and_initialize_constraints {
+
+namespace techniques {
+
+// \name Constructors and destructor
+//{
+
+ChainOfSprings::ChainOfSprings(mundy::mesh::BulkData *const bulk_data_ptr_, const Teuchos::ParameterList &fixed_params)
+    : bulk_data_ptr_(bulk_data_ptr_), meta_data_ptr_(&bulk_data_ptr_->mesh_meta_data()) {
+  // The bulk data pointer must not be null.
+  MUNDY_THROW_ASSERT(bulk_data_ptr_ != nullptr, std::invalid_argument,
+                     "ChainOfSprings: bulk_data_ptr_ cannot be a nullptr.");
+
+  // Validate the input params. Use default values for any parameter not given.
+  Teuchos::ParameterList valid_fixed_params = fixed_params;
+  valid_fixed_params.validateParametersAndSetDefaults(ChainOfSprings::get_valid_fixed_params());
+
+  // Get the field pointers.
+  const std::string node_coord_field_name = mundy::constraints::HookeanSprings::get_node_coord_field_name();
+
+  node_coord_field_ptr_ = meta_data_ptr_->get_field<double>(stk::topology::NODE_RANK, node_coord_field_name);
+
+  MUNDY_THROW_ASSERT(node_coord_field_ptr_ != nullptr, std::invalid_argument,
+                     "ChainOfSprings: node_coord_field_ptr cannot be a nullptr. Check that the field exists.");
+
+  // Get the part pointers.
+  const Teuchos::Array<std::string> spring_part_names =
+      valid_fixed_params.get<Teuchos::Array<std::string>>("spring_part_names");
+  auto parts_from_names = [](mundy::mesh::MetaData &meta_data,
+                             const Teuchos::Array<std::string> &part_names) -> std::vector<stk::mesh::Part *> {
+    std::vector<stk::mesh::Part *> parts;
+    for (const std::string &part_name : part_names) {
+      std::cout << "part_name: " << part_name << std::endl;
+      stk::mesh::Part *part = meta_data.get_part(part_name);
+      MUNDY_THROW_ASSERT(part != nullptr, std::invalid_argument,
+                         "ChainOfSprings: Expected a part with name '" << part_name << "' but part does not exist.");
+      parts.push_back(part);
+    }
+    return parts;
+  };
+
+  spring_part_ptrs_ = parts_from_names(*meta_data_ptr_, spring_part_names);
+}
+//}
+
+// \name Setters
+//{
+
+void ChainOfSprings::set_mutable_params(const Teuchos::ParameterList &mutable_params) {
+  // Validate the input params. Use default values for any parameter not given.
+  Teuchos::ParameterList valid_mutable_params = mutable_params;
+  valid_mutable_params.validateParametersAndSetDefaults(ChainOfSprings::get_valid_mutable_params());
+
+  num_nodes_ = valid_mutable_params.get<size_t>("num_nodes");
+  num_springs_ = num_nodes_ - 1;
+  coordinate_map_ptr_ = valid_mutable_params.get<std::shared_ptr<ArchlengthCoordinateMapping>>("coordinate_mapping");
+}
+//}
+
+// \name Getters
+//{
+
+stk::mesh::EntityId ChainOfSprings::get_node_id(const size_t &sequential_node_index) const {
+  return node_id_start_ + sequential_node_index;
+}
+
+stk::mesh::EntityId ChainOfSprings::get_element_id(const size_t &sequential_element_index) const {
+  return element_id_start_ + sequential_element_index;
+}
+
+std::pair<stk::mesh::EntityId, stk::mesh::EntityId> ChainOfSprings::get_connected_node_ids(
+    const size_t &sequential_element_index) const {
+  return {get_node_id(sequential_element_index), get_node_id(sequential_element_index + 1)};
+}
+
+stk::mesh::Entity ChainOfSprings::get_node(const size_t &sequential_node_index) const {
+  return bulk_data_ptr_->get_entity(stk::topology::NODE_RANK, get_node_id(sequential_node_index));
+}
+
+stk::mesh::Entity ChainOfSprings::get_element(const size_t &sequential_element_index) const {
+  return bulk_data_ptr_->get_entity(stk::topology::ELEMENT_RANK, get_element_id(sequential_element_index));
+}
+
+std::pair<stk::mesh::Entity, stk::mesh::Entity> ChainOfSprings::get_connected_nodes(
+    const size_t &sequential_element_index) const {
+  return {get_node(sequential_element_index), get_node(sequential_element_index + 1)};
+}
+//}
+
+// \name Actions
+//{
+
+void ChainOfSprings::execute() {
+  // Create the springs and their connected nodes, distributing the work across the ranks.
+  const size_t rank = bulk_data_ptr_->parallel_rank();
+  const size_t nodes_per_rank = num_nodes_ / bulk_data_ptr_->parallel_size();
+  const size_t remainder = num_nodes_ % bulk_data_ptr_->parallel_size();
+  const size_t start_node_index = rank * nodes_per_rank + std::min(rank, remainder);
+  const size_t end_node_index = start_node_index + nodes_per_rank + (rank < remainder ? 1 : 0);
+
+  bulk_data_ptr_->modification_begin();
+  openrand::Philox rng(1, 0);
+  for (size_t i = start_node_index; i < end_node_index; ++i) {
+    // Create the node.
+    stk::mesh::EntityId our_node_id = get_node_id(i);
+    stk::mesh::Entity node = bulk_data_ptr_->declare_node(our_node_id);
+    bulk_data_ptr_->change_entity_parts(node, spring_part_ptrs_);
+
+    // Set the node's coordinates using the given coordinate map.
+    double *const node_coords = stk::mesh::field_data(*node_coord_field_ptr_, node);
+    const auto [coord_x, coord_y, coord_z] = coordinate_map_ptr_->get_grid_coordinate({i});
+    node_coords[0] = coord_x;
+    node_coords[1] = coord_y;
+    node_coords[2] = coord_z;
+  }
+
+  // Share the nodes with the neighboring ranks.
+  // Note, node sharing is symmetric. If we don't own the node that we intend to share, we need to declare it before
+  // marking it as shared. If we are rank 0, we share our final node with rank 1 and receive their first node. If we are
+  // rank N, we share our first node with rank N - 1 and receive their final node. Otherwise, we share our first and
+  // last nodes with the corresponding neighboring ranks and receive their corresponding nodes.
+  if (bulk_data_ptr_->parallel_size() > 1) {
+    if (rank == 0) {
+      // Share the last node with rank 1.
+      stk::mesh::Entity node = get_node(end_node_index - 1);
+      bulk_data_ptr_->add_node_sharing(node, rank + 1);
+
+      // Receive the first node from rank 1
+      stk::mesh::EntityId received_node_id = get_node_id(end_node_index);
+      stk::mesh::Entity received_node = bulk_data_ptr_->declare_node(received_node_id);
+      bulk_data_ptr_->add_node_sharing(received_node, rank + 1);
+    } else if (rank == bulk_data_ptr_->parallel_size() - 1) {
+      // Share the first node with rank N - 1.
+      stk::mesh::Entity node = get_node(start_node_index);
+      bulk_data_ptr_->add_node_sharing(node, rank - 1);
+
+      // Receive the last node from rank N - 1.
+      stk::mesh::EntityId received_node_id = get_node_id(start_node_index - 1);
+      stk::mesh::Entity received_node = bulk_data_ptr_->declare_node(received_node_id);
+      bulk_data_ptr_->add_node_sharing(received_node, rank - 1);
+    } else {
+      // Share the first and last nodes with the corresponding neighboring ranks.
+      stk::mesh::Entity first_node = get_node(start_node_index);
+      stk::mesh::Entity last_node = get_node(end_node_index - 1);
+      bulk_data_ptr_->add_node_sharing(first_node, rank - 1);
+      bulk_data_ptr_->add_node_sharing(last_node, rank + 1);
+
+      // Receive the corresponding nodes from the neighboring ranks.
+      stk::mesh::EntityId received_first_node_id = get_node_id(start_node_index - 1);
+      stk::mesh::EntityId received_last_node_id = get_node_id(end_node_index);
+      stk::mesh::Entity received_first_node = bulk_data_ptr_->declare_node(received_first_node_id);
+      stk::mesh::Entity received_last_node = bulk_data_ptr_->declare_node(received_last_node_id);
+      bulk_data_ptr_->add_node_sharing(received_first_node, rank - 1);
+      bulk_data_ptr_->add_node_sharing(received_last_node, rank + 1);
+    }
+  }
+
+  // Now the springs.
+  const size_t start_spring_index = start_node_index;
+  const size_t end_spring_index = end_node_index - 1;
+  for (size_t i = start_spring_index; i < end_spring_index; ++i) {
+    // Create the spring.
+    stk::mesh::EntityId spring_id = get_element_id(i);
+    stk::mesh::Entity spring = bulk_data_ptr_->declare_element(spring_id);
+    bulk_data_ptr_->change_entity_parts(spring, spring_part_ptrs_);
+
+    // Create the node and connect it to the spring.
+    auto [node0, node1] = get_connected_nodes(i);
+    bulk_data_ptr_->declare_relation(spring, node0, 0);
+    bulk_data_ptr_->declare_relation(spring, node1, 1);
+
+    // Set the node's coordinates using the given coordinate map.
+    double *const node0_coords = stk::mesh::field_data(*node_coord_field_ptr_, node0);
+    double *const node1_coords = stk::mesh::field_data(*node_coord_field_ptr_, node1);
+    const auto [coord0_x, coord0_y, coord0_z] = coordinate_map_ptr_->get_grid_coordinate({i});
+    const auto [coord1_x, coord1_y, coord1_z] = coordinate_map_ptr_->get_grid_coordinate({i + 1});
+    node0_coords[0] = coord0_x;
+    node0_coords[1] = coord0_y;
+    node0_coords[2] = coord0_z;
+    node1_coords[0] = coord1_x;
+    node1_coords[1] = coord1_y;
+    node1_coords[2] = coord1_z;
+  }
+  bulk_data_ptr_->modification_end();
+}
+//}
+
+}  // namespace techniques
+
+}  // namespace declare_and_initialize_constraints
+
+}  // namespace constraints
+
+}  // namespace mundy
